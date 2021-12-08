4a2f65772189066aadb33950277305a32e01ccf9
0bject / ts5h <at4@jc4.so-net.ne.jp>
2021-12-08 11:40:05 +0100

    Fix lint errors on Canvas.ts

--- src/class/Canvas.ts
+++ src/class/Canvas.ts
@@ -1,41 +1,38 @@
 export default class Canvas {
-  private canvas: any;
-  private readonly ctx: any;
-  private wrapper: any;
+  private wrapper: HTMLDivElement;
+  private canvas: HTMLCanvasElement;
+  private readonly ctx: CanvasRenderingContext2D | null;
 
   constructor(elementID: string) {
-    this.canvas = document.getElementById(elementID)
-    this.wrapper = this.canvas.closest("div")
-    this.ctx = null
+    this.canvas = document.getElementById(elementID) as HTMLCanvasElement;
+    this.wrapper = this.canvas.closest("div") as HTMLDivElement;
+    this.ctx = null;
 
-    this.initCanvas()
+    this.initCanvas();
   }
 
   initCanvas(): void {
-    this.resizeCanvas()
-
-    // 3D
-    //this.ctx = this.canvas.getContext('2d');
+    this.resizeCanvas();
   }
 
   resizeCanvas(): void {
-    const ww = this.wrapper.offsetWidth.toString()
-    const wh = this.wrapper.offsetHeight.toString()
-    this.canvas.setAttribute("width", ww)
-    this.canvas.setAttribute("height", wh)
-    this.canvas.style.width = ww + "px"
-    this.canvas.style.height = wh + "px"
+    const ww = this.wrapper.offsetWidth.toString();
+    const wh = this.wrapper.offsetHeight.toString();
+    this.canvas.setAttribute("width", ww);
+    this.canvas.setAttribute("height", wh);
+    this.canvas.style.width = ww + "px";
+    this.canvas.style.height = wh + "px";
   }
 
   getWindowWidth(): number {
-    return this.canvas.offsetWidth
+    return this.canvas.offsetWidth;
   }
 
-  getWindowHeight(): number{
-    return this.canvas.offsetHeight
+  getWindowHeight(): number {
+    return this.canvas.offsetHeight;
   }
 
-  getContext(): HTMLCanvasElement {
-    return this.ctx;
+  getContext() {
+    return <CanvasRenderingContext2D>this.ctx;
   }
 }
